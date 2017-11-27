81dc4c99867c334118f0a3370a6b926ab7d0e46a
petikun <ara.karai2@gmail.com>
2017-11-27 11:49:27 +0100

    Komentovanie ydrojoveho kodu

--- src/create_network.py
+++ src/create_network.py
@@ -1,56 +1,38 @@
-"""
-minievents is a Framework over Mininet to introduce an event generator.
-Events are changes in mininet network at a specific time. The events are defined                                                                                                              in a json document.
-Implemented events are traffic generation (iperf) and link variations
-(bandwidth, loss, delay) during a mininet launch.
-It will launch mininet and perform the events at the specific time of each event
-"""
+
 
 import time
 import json
 import argparse
-import os
 from time import sleep
-
-from minisched import scheduler
-#from mininet.cli import CLI
-from mininet.topo import SingleSwitchTopo
-from mininet.log import setLogLevel, info, debug
-from mininet.net import Mininet
-from mininet.node import OVSController, DefaultController, Host, OVSKernelSwitch
-from mininet.link import TCLink, Intf, Link
-
-
-
 import os
 import re
 import sys
 import argparse
 from mininet.node import Node
 from mininet.cli import CLI
-from mininet.log import setLogLevel, info, error
-from mininet.net import Mininet
-from mininet.link import Intf,TCLink,Link
 from mininet.topolib import TreeTopo
 from mininet.util import quietRun
-from mininet.node import OVSKernelSwitch,Host,DefaultController
 from mininet.topo import Topo
+from minisched import scheduler
+from mininet.topo import SingleSwitchTopo
+from mininet.log import setLogLevel, info, debug,error
+from mininet.net import Mininet
+from mininet.node import OVSController, DefaultController, Host, OVSKernelSwitch
+from mininet.link import TCLink, Intf, Link
 
 class Min( Topo ):
     "Minimal topology with a single switch and two hosts"
 
     def build( self ):
-        # vytvorenie switch
+        # Create a switch
         from mininet.topo import Topo
 
         s1=self.addSwitch('s1')
-        #vytvorenie root node
-		root=self.addNode('root',ip='192.168.2.254/24',inetIntf='eth0',inNamespa                                                                                                             ce=False)
+        root=self.addNode('root',ip='192.168.2.254/24',inetIntf='eth0',inNamespace=False)
         self.addLink(s1,root,bw=100)
-       
 
 
-# mininevents pre bandiwth shaping
+
 class Minievents(Mininet):
     def __init__(self, topo=None, switch=OVSKernelSwitch, host=Host,
                  controller=DefaultController, link=Link, intf=Intf,
@@ -58,10 +40,10 @@ class Minievents(Mininet):
                  inNamespace=False,
                  autoSetMacs=False, autoStaticArp=False, autoPinCpus=False,
                  listenPort=None, waitConnected=False, events_file=None):
-        super(Minievents, self).__init__(topo=topo, switch=switch, host=host, co                                                                                                             ntroller=controller,
-                                         link=link, intf=intf, build=build, xter                                                                                                             ms=xterms, cleanup=cleanup,
-                                         ipBase=ipBase, inNamespace=inNamespace,                                                                                                              autoSetMacs=autoSetMacs,
-                                         autoStaticArp=autoStaticArp, autoPinCpu                                                                                                             s=autoPinCpus,
+        super(Minievents, self).__init__(topo=topo, switch=switch, host=host, controller=controller,
+                                         link=link, intf=intf, build=build, xterms=xterms, cleanup=cleanup,
+                                         ipBase=ipBase, inNamespace=inNamespace, autoSetMacs=autoSetMacs,
+                                         autoStaticArp=autoStaticArp, autoPinCpus=autoPinCpus,
                                          listenPort=listenPort,
                                          waitConnected=waitConnected)
 
@@ -70,22 +52,30 @@ class Minievents(Mininet):
             json_events = json.load(open(events_file))
             self.load_events(json_events)
 
-    def load_events(self, json_events):#nacitanie z 	Json
-        
+    def load_events(self, json_events):
+
         event_type_to_f = {'editLink': self.editLink}
         for event in json_events:
-            debug("processing event: time {time}, type {type}, params {params}\n                                                                                                             ".format(**event))
+            debug("processing event: time {time}, type {type}, params {params}\n".format(**event))
             event_type = event['type']
-            self.scheduler.enter(event['time'], 1, event_type_to_f[event_type],                                                                                                              kwargs=event['params'])
+            self.scheduler.enter(event['time'], 1, event_type_to_f[event_type], kwargs=event['params'])
 
-   
+    # edit link event
 
 
-    def editLink(self, **kwargs):# funkcia pre bandwith shaping
-        
+    def editLink(self, **kwargs):
+        """
+        Command to edit the properties of a link between src and dst.
+        :param kwargs: named arguments
+            src: name of the source node.
+            dst: name of the destination node.
+            bw: bandwidth in Mbps.
+            loss: packet loss ratio percentage.
+            delay: delay in ms.
+        """
         n1, n2 = self.get(kwargs['src'], kwargs['dst'])
         intf_pairs = n1.connectionsTo(n2)
-        info('***editLink event at t={time}: {args}\n'.format(time=time.time(),                                                                                                              args=kwargs))
+        info('***editLink event at t={time}: {args}\n'.format(time=time.time(), args=kwargs))
         for intf_pair in intf_pairs:
             n1_intf, n2_intf = intf_pair
             n1_intf.config(**kwargs)
@@ -100,18 +90,18 @@ class Minievents(Mininet):
 
 if __name__ == '__main__':
 
-    os.system("sudo ifconfig eth0 192.168.1.254 netmask 255.255.255.0 up")#OS routing
-    os.system("sudo ip route add 192.168.2.254 dev eth0")#OS routing
-    os.system("sudo sysctl net.ipv4.conf.all.forwarding=1")#OS forwarding
+    os.system("sudo ifconfig eth0 192.168.1.254 netmask 255.255.255.0 up")
+    os.system("sudo ip route add 192.168.2.254 dev eth0")
+    os.system("sudo sysctl net.ipv4.conf.all.forwarding=1")
 
-    parser = argparse.ArgumentParser()#minievents priprava
-    parser.add_argument("--events",default="bw.json", help="json file with event                                                                                                              descriptions")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--events",default="bw.json", help="json file with event descriptions")
     args = parser.parse_args()
     setLogLevel('info')
     net = Minievents( topo=Min( ),link=TCLink, events_file=args.events)
     intfName ='eth1'
     switch = net.switches[ 0 ]
-    _intf = Intf( intfName, node=switch)#pridanie interface eth1
+    _intf = Intf( intfName, node=switch)
 
     net.start()
     CLI( net )
